a1b67001297d3fc35e6c5c5c81a387b62a8ee406
LKz666 <lucasedu292003@gmail.com>
2025-02-09 14:01:04 -0300

    Usando encapsulamento

--- Mercado/Mercado/Estoque.cs
+++ Mercado/Mercado/Estoque.cs
@@ -2,28 +2,63 @@
 {
     internal class Estoque
     {
-        public string Nome;
-        public double Valor;
-        public int Quantidade;
+        private string _nome;
+        private double _valor;
+        private int _quantidade;
+
+        public Estoque(string nome, double valor, int quantidade){
+            _nome = nome;
+            _valor = valor;
+            _quantidade = quantidade;
+        }
+
+        // public string Nome;
+        // public double Valor;
+        // public int Quantidade;
+
+        public string GetNome(){
+            return _nome;
+        }
+
+        public void SetNome(string nome){
+            _nome = nome;
+        }
+
+        public double GetValor(){
+            return _valor;
+        }
+
+        public void SetValor(double valor){
+            _valor = valor;
+        }
+
+        public int GetQuantidade(){
+            return _quantidade;
+        }
+
+        public void SetQuantidade(int quantidade){
+            _quantidade = quantidade;
+        }
+
 
         public double ValorTotal()
         {
-            return Valor * Quantidade;
+            return _valor * _quantidade;
         }
 
         public void AdicionarProduto(int quantidade) 
         { 
-            Quantidade += quantidade;
+            _quantidade += quantidade;
         }
 
         public void RemoverProduto(int quantidade) 
         { 
-            Quantidade -= quantidade;
+            _quantidade -= quantidade;
         }
 
         public override string ToString()
         {
-            return Nome + " " + Valor + " " + Quantidade + " " + ValorTotal();
+            return _nome + " " + _valor + " " + _quantidade + " " + ValorTotal();
         }
     }
 }
--- Mercado/Mercado/Program.cs
+++ Mercado/Mercado/Program.cs
@@ -6,28 +6,36 @@ namespace Mercado
     {
         static void Main(string[] args) 
         {
-            Estoque produto = new Estoque();
+            Estoque produto = new Estoque("TV", 1500.00, 15);
 
-            Console.Write("Digite o nome do produto : ");
-            produto.Nome = Console.ReadLine();
+            produto.SetNome("TV 4K");
+            produto.SetValor(2000.00);
+            produto.SetQuantidade(10);
 
-            Console.Write("Digite o valor do produto : ");
-            produto.Valor = double.Parse(Console.ReadLine());
+            Console.WriteLine(produto.GetNome());
+            Console.WriteLine(produto.GetValor());
+            Console.WriteLine(produto.GetQuantidade());
 
-            Console.Write("Digite a quantidade no estoque : ");
-            produto.Quantidade = int.Parse(Console.ReadLine());
+            // Console.Write("Digite o nome do produto : ");
+            // produto.Nome = Console.ReadLine();
 
-            Console.WriteLine("Dados do produto: " + produto);
+            // Console.Write("Digite o valor do produto : ");
+            // produto.Valor = double.Parse(Console.ReadLine());
 
-            Console.Write("Quatidade de produtos adicionados no estoque : ");
-            int quantidade = int.Parse(Console.ReadLine());
-            produto.AdicionarProduto(quantidade);
-            Console.WriteLine("Dados atualizados: " + produto);
+            // Console.Write("Digite a quantidade no estoque : ");
+            // produto.Quantidade = int.Parse(Console.ReadLine());
 
-            Console.Write("Quatidade de produtos removidos no estoque : ");
-            quantidade = int.Parse(Console.ReadLine());
-            produto.RemoverProduto(quantidade);
-            Console.WriteLine("Dados atualizados: " + produto);
+            // Console.WriteLine("Dados do produto: " + produto);
+
+            // Console.Write("Quatidade de produtos adicionados no estoque : ");
+            // int quantidade = int.Parse(Console.ReadLine());
+            // produto.AdicionarProduto(quantidade);
+            // Console.WriteLine("Dados atualizados: " + produto);
+
+            // Console.Write("Quatidade de produtos removidos no estoque : ");
+            // quantidade = int.Parse(Console.ReadLine());
+            // produto.RemoverProduto(quantidade);
+            // Console.WriteLine("Dados atualizados: " + produto);
         }
     }
 }
